bf5f9780048fa8ae42d7f37c8ebbc5b099b121d6
Kunj Patel <kunjkumar6151@gmail.com>
2026-06-12 15:35:04 +0530

    Integrate Firebase with CampusTrace

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
-// main.dart
-
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'firebase_options.dart';
 import 'package:campus_trace/frontend/screens/home/splash_loading_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const CampusTraceApp());
 }
 
